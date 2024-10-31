758224793c4604a530cdb235bf69611c93b4245d
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-10-31 17:28:22 +0300

    5.6

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Notification from "./components/Notification"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
 import loginService from "./services/login"
+import Togglable from "./components/Togglable"
+import CreateBlogForm from "./components/CreateForm"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -11,11 +13,6 @@ const App = () => {
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [notificationStatus, setNotificationStatus] = useState(false)
   const [user, setUser] = useState(null)
-  const [title, setTitle] = useState("")
-  const [author, setAuthor] = useState("")
-  const [url, setUrl] = useState("")
-
-  const [loginVisible, setLoginVisible] = useState(false)
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -72,21 +69,11 @@ const App = () => {
     }, 5000)
   }
 
-  const handleCreate = async (event) => {
-    event.preventDefault()
-
+  const addBlog = async (blogObject) => {
     try {
-      const createdBlog = await blogService.create({
-        author,
-        title,
-        url,
-      })
+      const createdBlog = await blogService.create(blogObject)
 
       setBlogs(blogs.concat(createdBlog))
-      setTitle("")
-      setAuthor("")
-      setUrl("")
-
       setNotificationMessage(
         `a new blog ${createdBlog.title} by ${createdBlog.author} added`
       )
@@ -130,50 +117,10 @@ const App = () => {
   )
 
   const createForm = () => {
-    const hideWhenVisible = { display: loginVisible ? "none" : "" }
-    const showWhenVisible = { display: loginVisible ? "" : "none" }
-
     return (
-      <div>
-        <div style={hideWhenVisible}>
-          <button onClick={() => setLoginVisible(true)}>create note</button>
-        </div>
-
-        <div style={showWhenVisible}>
-          <h2>create new</h2>
-          <form onSubmit={handleCreate}>
-            <div>
-              title
-              <input
-                type="text"
-                value={title}
-                name="title"
-                onChange={({ target }) => setTitle(target.value)}
-              />
-            </div>
-            <div>
-              author
-              <input
-                type="text"
-                value={author}
-                name="author"
-                onChange={({ target }) => setAuthor(target.value)}
-              />
-            </div>
-            <div>
-              url
-              <input
-                type="text"
-                value={url}
-                name="url"
-                onChange={({ target }) => setUrl(target.value)}
-              />
-            </div>
-            <button type="submit">create</button>
-          </form>
-          <button onClick={() => setLoginVisible(false)}>cancel</button>
-        </div>
-      </div>
+      <Togglable buttonLabel="new blog">
+        <CreateBlogForm createBlog={addBlog} />
+      </Togglable>
     )
   }
 
