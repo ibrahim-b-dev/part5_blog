9114a3969eda53c3fd01044e86df5d24d4b5b9cb
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-10-31 18:38:11 +0300

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const changeVisibility = () => {
+    console.log("cahnges visible", visible)
+
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={changeVisibility}>view</button>
+      </div>
+
+      <div style={{ display: visible ? "" : "none" }}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button onClick={() => null}>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
